b16e5b21a896350081695c4b636b8ea6d941a74c
Andy Sit <asit5@uwo.ca>
2024-04-05 02:34:09 -0400

    fixes to the viewpointPosition calculator

--- experiment/camera_surface.py
+++ experiment/camera_surface.py
@@ -75,12 +75,12 @@ class Camera:
         pass
 
     def viewpointPosition(self):
-        #goal to give a 2d vector which points to the location of self.focus.pos
-        heading = self.focus.pos - self.viewP
-        self.viewP += heading * 0.05
-        goto = -self.viewP + self.origin
-        print(goto)
-        return goto
+        # Calculate the difference between the player and the center of the screen
+        heading = self.focus.pos - self.origin
+        # Move the camera gradually towards the player
+        self.origin += heading * 0.05
+        return -self.origin + Vector2(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
 
     def update(self):
         #calculate the difference between this camera and enity
