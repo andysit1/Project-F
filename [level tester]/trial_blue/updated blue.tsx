57ca21c20eb14ab2fbb5b6533bb308c53e2b5994
Luca <madman5604@gmail.com>
2024-04-21 22:09:09 -0400

    fixed tsx formatting with new tileset

--- [level tester]/trial_blue/updated blue.tsx
+++ [level tester]/trial_blue/updated blue.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="updated blue" tilewidth="16" tileheight="16" tilecount="45" columns="9">
- <image source="./updated blue.png" width="144" height="80"/>
+<tileset version="1.10" tiledversion="1.10.2" name="updated blue" tilewidth="16" tileheight="16" tilecount="46" columns="10">
+ <image source="./updated blue.png" width="160" height="80"/>
 </tileset>
