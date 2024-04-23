1fff92e86879c4b84e510c7c23c2758ca639727a
Andy Sit <andysit173@gmail.com>
2024-04-22 21:55:05 -0400

    changed viewpointPosition logic to base off of player position instead of center of screen

--- src/components/camera.py
+++ src/components/camera.py
@@ -2,14 +2,12 @@ import pygame as pg
 from pygame.math import Vector2
 from components.player import Player
 
-#binds to player -> focuses on player or maybe an object?!?!
 
 # This is the camera class handles the position of surface we see on the world surface
 class Camera:
     def __init__(self, focus):
         #focus/lock on player
         self.focus : Player = focus
-        self.view : pg.Surface = pg.display.set_mode((800, 600))
         self.origin = Vector2(800 // 2, 600 //2)
         self.viewP = self.origin.copy()
 
@@ -18,4 +16,4 @@ class Camera:
         heading = self.focus.pos - self.origin
         # Move the camera gradually towards the player
         self.origin += heading * 0.05
-        return -self.origin + Vector2(800 // 2, 600 // 2)
+        return -self.origin + self.focus.pos
