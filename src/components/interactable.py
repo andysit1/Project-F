506d32e2290592b2e3589ac1f3a3aff112a961f6
Andy Sit <andysit173@gmail.com>
2024-07-02 06:39:24 -0400

    3 main interactions outline

--- src/components/interactable.py
+++ src/components/interactable.py
@@ -11,12 +11,13 @@ import pygame as pg
 
 
 '''
-  --- Sprite class ---
+  --- Interactions class ---
   This class is responsible for handling the basic collision logic
 
   Functions:
     __init__ : Initializes the UI object
-    lock_on
+
+
 
   Usage Idea:
   You give all Interactions a reference to the player rect so we can calc the collisions. Each object
@@ -25,11 +26,28 @@ import pygame as pg
 '''
 
 class Interactions(pg.sprite.Sprite):
-  def __init__(self, player, *groups) -> None:
+  def __init__(self, entity, *groups) -> None:
     super().__init__(*groups)
-    self.player_rect : pg.rect.Rect = player.rect
+    self.entity = entity
+    self.entity_rect : pg.rect.Rect = entity.rect
     self.image = None
     self.rect = None
+
+  #this is a useful function to know if an entity is going towards or away for more specific interactions
+  @classmethod
+  def is_moving_towards(self):
+    pos = self.entity.pos
+
+    center = pg.Vector2(self.rect.centerx, self.rect.centery)
+    next_step = pos + self.entity.vel
+
+    #if the next step given the current vel is closer to the center of an object(pushing against) then this means we are going towards
+    #else then we are going away
+    if center.distance_to(next_step) < center.distance_to(pos):
+      return True
+    else:
+      return False
+
 
 
   def update(self, dt):
@@ -41,7 +59,7 @@ class Interactions(pg.sprite.Sprite):
           delta: The time elapsed since the last update.
     """
 
-    if self.rect.colliderect(self.player_rect):
+    if self.rect.colliderect(self.entity_rect):
       self.on_event()
 
   def on_event(self):
@@ -54,25 +72,48 @@ class Interactions(pg.sprite.Sprite):
     pass
 
 
+'''
+  --- PushObject class ---
+  This class is responsible for handling the basic pushing logic
+
+  Idea:
+  In real life when we push objects the force which we apply will alway apply back in equal force,
+  this means that we need to code walls ingame/other objects that can move to shoot an object force backwards
+
+'''
+
+class WallObject(Interactions):
+  def __init__(self, entity, *groups) -> None:
+    super().__init__(entity, *groups)
+
+  def on_event(self):
+
+    if self.is_moving_towards:
+      self.entity -= self.entity.vel  #whenever is hit whether, pushobject or player object we apply the negative vel vectors backwards
+
+    return super().on_event()
+
+'''
+  --- PushObject class ---
+  This class is responsible for handling the basic pushing logic
+
+  Idea:
+  In real life when we push objects the force which we apply will alway apply back in equal force,
+  this means that we need to code walls ingame/other objects that can move to shoot an object force backwards
+
+'''
 
 class PushObject(Interactions):
-  def __init__(self, player, *groups) -> None:
-    super().__init__(player, *groups)
+  def __init__(self, entity, *groups) -> None:
+    super().__init__(entity, *groups)
     self.vel : pg.Vector2 = None
     self.pos : pg.Vector2 = None
 
   def on_event(self):
-    """
-      set self.vel = self.player.vel
-      apply the vel to the box pos
+    #push object
+    self.pos += self.entity.vel
+    self.rect.center = self.pos
 
-      OR
-
-      we can treat it like moving sprite ie like the attack sprites
-      then we save the position of the box based ont he self.selected_points
-    """
-
-    pass
 
 
 class DialogueObject(Interactions):
