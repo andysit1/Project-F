bdbab9c9f745f9288a040dc6c1efdcb9d13f7a75
Andy Sit <andysit173@gmail.com>
2024-07-02 05:59:07 -0400

    base of interactions

--- src/components/interactable.py
+++ src/components/interactable.py
@@ -6,13 +6,32 @@
 
 #the difference is the sprite, radius to interact, and the result from the interaction
 
+from typing import Any
 import pygame as pg
-from .ui import DialogueState
+from modules.state_machine import Machine
 
 
-class Interactable(pg.sprite.Sprite):
-  def __init__(self, *groups) -> None:
+'''
+  --- Sprite class ---
+  This class is responsible for handling the basic collision logic
+
+  Functions:
+    __init__ : Initializes the UI object
+    lock_on
+
+  Usage Idea:
+  You give all Interactions a reference to the player rect so we can calc the collisions. Each object
+  is used to handle logic for anything that the player can do inside a game. So in the future we will use this class
+  to manage our Pushable, Dialogue, and other classes
+'''
+
+class Interactions(pg.sprite.Sprite):
+  def __init__(self, player, *groups) -> None:
     super().__init__(*groups)
+    self.player_rect : pg.rect.Rect = player.rect
+    self.image = None
+    self.rect = None
+
 
   def update(self, dt):
     """
@@ -22,15 +41,13 @@ class Interactable(pg.sprite.Sprite):
       Args:
           delta: The time elapsed since the last update.
     """
-    pass
+
+    if self.rect.colliderect(self.player_rect):
+      self.on_event()
 
   def on_event(self):
     """
-    Method called when an event occurs.
-    Might trigger dialouge system
-    Might trigger item drop
-    Might trigger health increase not sure yet
-
+    Triggers the specific event given the interaction we want
     Args:
         event: The event object.
     """
@@ -38,6 +55,32 @@ class Interactable(pg.sprite.Sprite):
     pass
 
 
-class Interactable_Dialogueable(DialogueState):
-  def __init__(self):
-    super().__init__()
+
+class PushObject(Interactions):
+  def __init__(self, player, *groups) -> None:
+    super().__init__(player, *groups)
+    self.vel : pg.Vector2 = None
+    self.pos : pg.Vector2 = None
+
+  def on_event(self):
+    """
+      set self.vel = self.player.vel
+      apply the vel to the box pos
+
+      OR
+
+      we can treat it like moving sprite ie like the attack sprites
+      then we save the position of the box based ont he self.selected_points
+    """
+
+    pass
+
+
+class DialogueObject(Interactions):
+  def __init__(self, player, dialogue, dialogue_machine,*groups) -> None:
+    super().__init__(player, *groups)
+    self.dialogue_engine = dialogue_machine
+    self.dialogue = dialogue
+
+  def on_event(self):
+    self.dialogue_engine.dialogue_machine.add_dialogue(self.dialogue_test_state)
