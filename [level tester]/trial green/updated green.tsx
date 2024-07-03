4095a1629af139f2002a74509a00f021036c46ed
Luca <madman5604@gmail.com>
2024-07-02 22:53:26 -0400

    added the grasslands png, tsx, tmx files added the 1.0 ver of the blue map added dev tileset :heart:

---
+++ [level tester]/trial green/updated green.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="updated green" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="grasslands.png" width="160" height="80"/>
+ <tile id="13" probability="10"/>
+</tileset>
