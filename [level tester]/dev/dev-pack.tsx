4763b1265f7db8c42c919f16ef1242e09c042cf1
Luca <madman5604@gmail.com>
2024-07-05 16:09:50 -0400

    dev tileset implementation
    - added the dev-pack tileset png and tsx files
    - updated the dungeon/grass tmx files to use the dev packs
    - deleted old/unused assets
    - updated the grasslands/dungeons tilesets by removing the old dev tiles

---
+++ [level tester]/dev/dev-pack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="dev-pack" tilewidth="16" tileheight="16" tilecount="10" columns="5">
+ <image source="dev-pack.png" width="80" height="32"/>
+</tileset>
