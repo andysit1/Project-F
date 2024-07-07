bb0ddc736eae132620c4a685c3ee4da6eea710fe
Andy Sit <andysit173@gmail.com>
2024-07-07 10:32:25 -0400

    removed pierce attack

--- src/components/attack.py
+++ src/components/attack.py
@@ -17,8 +17,6 @@ class SweepAttackSprite(Moving_Sprite):
         self.attack_sprite = AttackSprite(focus, *groups)
         self.attack_timeout_timer = Timer(threshold=5) # amount of seconds before times out
 
-
-
     def handle_attack_input(self, groups : pg.sprite.Group):
         if self.attack_sequence == 0:
             self.perform_attack(groups)
@@ -84,6 +82,7 @@ class AttackSprite(Moving_Sprite):
                     #um im too lazy to work around the healthbar in enemy group
                     #this just stops errors from collisions of healthbars
                     pass
+
     def perform_smash_attack(self, groups : pg.sprite.Group):
         hit_list = pg.sprite.spritecollide(self, group=groups, dokill=False)
         for enemy in hit_list:
@@ -91,31 +90,6 @@ class AttackSprite(Moving_Sprite):
 
     def update(self, dt):
         return super().update(dt)
-
-#paused
-class PierceGrappleAttack(AttackSprite):
-    def __init__(self, focus, *groups) -> None:
-        super().__init__(focus, *groups)
-        self.attack_width = 100
-        self.reinit_hitbox(self.attack_width, self.attack_height)
-
-        self.distance = 0
-
-
-    def get_pierce_endpoint(self) -> float:
-        return pg.math.lerp(0, self.attack_width, self.distance)
-
-
-    #not updating the distance until we change from vertical to horizonal...
-    def update(self, dt):
-
-        self.distance += 0.1 * dt
-        if self.distance >= 1:
-            self.distance = 0
-        else:
-            endpoint = self.get_pierce_endpoint()
-            self.reinit_hitbox(endpoint, self.attack_height)   #the size of the tongue never changes
-            super().update(dt)
 
 
 class AttackHandler:
@@ -177,35 +151,3 @@ class AttackHandler:
     def clear_attack(self):
         self.attack_rect = pg.Rect(0, 0, 0, 0)  # Reset the attack_rect to an empty rectangle
         self.attack_sprite.clear_attack_visual()
-
-
-# class AttackSprite(pg.sprite.Sprite):
-#     def __init__(self, focus: AttackHandler, *groups):
-#         super().__init__(*groups)
-#         self.focus = focus
-#         self.attack_width = 20
-#         self.attack_height = 10
-#         self.image = pg.Surface([self.attack_height, self.attack_width])
-#         self.image.fill("red")
-#         self.image.set_alpha(0)
-#         self.rect = self.image.get_rect()  # Initialize rect with a valid pygame.Rect object
-
-#     def update(self, attack_rect=None):
-#         if attack_rect:
-#             if isinstance(attack_rect, (list, tuple)):
-#                 self.rect = pg.Rect(*attack_rect)  # Create a new Rect from the attack_rect tuple
-#             elif isinstance(attack_rect, pg.Rect):
-#                 self.rect = attack_rect  # If attack_rect is already a Rect, assign it directly
-#             else:
-#                 # Handle other data types as needed
-#                 pass
-#             if self.focus.game_state.player.direction in ("up", "down"):
-#                 self.image = pg.Surface([self.attack_height, self.attack_width])
-#             else:
-#                 self.image = pg.Surface([self.attack_width, self.attack_height])
-#             self.image.set_alpha(255)
-
-#     def clear_attack_visual(self):
-#         """Clear the attack rectangle after the attack is complete."""
-#         self.image.set_alpha(0)
-#         self.rect = self.image.get_rect()
