5f9436b988bdb09978dd363f99b5818ecdca46b4
Andy Sit <andysit173@gmail.com>
2024-07-12 11:21:55 -0400

    lighting manager with bugs - will fix ina bit

--- src/modules/lighting_manager.py
+++ src/modules/lighting_manager.py
@@ -12,15 +12,98 @@ we will have a cache for all glow surfaces given a size to reduce surface genera
 on update -> pass the glowable objects rects to light manager
 on_draw -> draws all lighting objects based on translation func and if they are within range
           then we apply lighting surface with blend mode before drawing ui component
+
+FOR NOW LETS JUST DO CIRCLE GLOW OBJECTS
+
+
 """
+
+
+import pygame as pg
+
+#fluffy potato code
+def circle_surf(radius, color):
+    surf = pg.Surface((radius * 2, radius * 2))
+    pg.draw.circle(surf, color, (radius, radius), radius)
+    surf.set_colorkey((0, 0, 0))
+    return surf
 
 
 class LightingManager():
     def __init__(self):
-      self.light_cache = ()
+      self.light_cache : list[pg.Rect] = []
+      self.glow_cache = dict()
+      self.light_layer : pg.Surface = pg.Surface((1920, 1080), pg.SRCALPHA)
 
-    def glow_color(self, pos, size, color):
-      pass
+    def create_glow_surface_cache(self):
+      for keys in self.glow_cache.keys():
+        if self.glow_cache[keys] == None:
+          self.glow_cache[keys] = circle_surf(keys[0] * 2, (20, 20, 60))
+      print(len(self.glow_cache))
+
+    #SET reference to list of rects
+    def set_glow_objects(self, glow_group : list[pg.rect.Rect]):
+      self.light_cache += glow_group
+      size_of_glow = self.glow_cache.keys()
+      #INIT dict keys with new sizes
+      for rect in glow_group:
+          if rect.size not in size_of_glow:
+            self.glow_cache[rect.size] = None #instead of size we should use a string representing size * color ()
+
+      self.create_glow_surface_cache()
+      print("size of glow", len(size_of_glow))
+
+    #draw the light surface (
+    def draw(self, surface : pg.Surface):
+      for obj in self.light_cache:
+        self.light_layer.blit(self.glow_cache[obj.size], (obj.centerx - obj.width * 2, obj.centery - obj.height * 2),  special_flags=pg.BLEND_RGB_ADD)
+      surface.blit(self.light_layer, (0, 0))
 
 
 
+"""
+FLOW
+
+lm.set_glow_objects(pygame group of sprites)
+
+  loop through and grab size of all rects and push to set if we have multiple of the same glow size then it doesnt matter
+
+
+
+"""
+
+
+if __name__ == "__main__":
+
+  import unittest
+
+  # Assuming LightingManager class and circle_surf function are defined here as provided
+
+  class TestLightingManager(unittest.TestCase):
+      def setUp(self):
+          pg.init()
+          self.lighting_manager = LightingManager()
+
+      def test_initialization(self):
+          self.assertIsInstance(self.lighting_manager.light_cache, list)
+          self.assertIsInstance(self.lighting_manager.glow_cache, dict)
+          self.assertIsInstance(self.lighting_manager.light_layer, pg.Surface)
+          self.assertEqual(self.lighting_manager.light_layer.get_size(), (1920, 1080))
+          self.assertEqual(self.lighting_manager.light_layer.get_flags() & pg.SRCALPHA, pg.SRCALPHA)
+
+
+      def test_rect_cache(self):
+        #start 10 objects in cache
+        rect_group = []
+        for i in range(10):
+          rect_group.append(pg.rect.Rect(i, i, i, i))
+        self.lighting_manager.set_glow_objects(rect_group)
+
+        #should only add 5 objects cause cache
+        rect_group = []
+        for i in range(5, 15):
+          rect_group.append(pg.rect.Rect(i, i, i, i))
+        self.lighting_manager.set_glow_objects(rect_group)
+
+  if __name__ == '__main__':
+      unittest.main()
--- src/state/dev_state.py
+++ src/state/dev_state.py
@@ -3,7 +3,9 @@
 
 import pygame as pg
 from components.player import Player
+from modules.lighting_manager import LightingManager
 from modules.state_machine import State
+
 
 '''
   --- GameState class ---
@@ -27,11 +29,23 @@ class DevState(State):
     self.dt = None
 
 
+    self.glow_rects : list[pg.Rect] = [pg.Rect(100, 100, 30, 30), pg.Rect(90, 110, 30, 30)]
+    self.light_manager = LightingManager()
+    self.light_manager.set_glow_objects(self.glow_rects)
+
+
+  def draw_test_rects(self):
+    return [pg.draw.rect(self.engine.surface, "BLUE", rect=rect) for rect in self.glow_rects]
   # What is done on each frame when drawn
   def on_draw(self):
-    self.engine.surface.fill((255, 255, 255))
+    self.engine.surface.fill((0,0,0))
     self.spites.draw(self.engine.surface)
     self.player.on_draw_player_sprites(self.engine.surface, None)
+    self.draw_test_rects()
+    self.light_manager.draw(self.engine.surface)
+
+
+
 
   # Updates relevant game state information
   def on_update(self, delta):
