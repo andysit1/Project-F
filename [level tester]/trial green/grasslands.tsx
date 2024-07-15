6bafbbf7143c210e903dff77f13069d591e4c42e
Luca <madman5604@gmail.com>
2024-07-15 14:15:32 -0400

    grasslands map + reorganizing assets

---
+++ [level tester]/trial green/grasslands.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="updated green" tilewidth="16" tileheight="16" tilecount="50" columns="6">
+ <image source="grasslands.png" width="96" height="80"/>
+ <tile id="8" probability="10"/>
+ <tile id="9" probability="10"/>
+</tileset>
